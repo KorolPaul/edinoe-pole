a40e32ca18341c8c217ace937fd5396fb8802c49
p_korol <korol.paul@gmail.com>
2019-08-15 15:54:18 +0300

    fx

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-    var isMobile = 'ontouchstart' in window || navigator.msMaxTouchPoints;
-
     /* Menu toggle */
     const $menu = $('.menu');
     const $menuToggle = $('.js-menu-toggle');
@@ -24,4 +22,49 @@ $(document).ready(function () {
 
     $('.fade').on('click', togglePopup);
 
+
+    /* Datapicker */
+    $('.datapicker').datepicker();
+
+    /* Custom select */
+    $('.select').selectize({
+        create: true,
+        sortField: 'text'
+    });
+
+    /* SVG animation */
+    setInterval(function() {
+        var $circles = $('.st0');
+        var circlesCount = $circles.length;
+
+        function getRandomArbitrary() {
+            return parseInt(Math.random() * (circlesCount));
+        }
+
+        var index = getRandomArbitrary();
+
+        $circles.eq(index).addClass('st1');
+        $circles.eq(index).removeClass('st0');
+        $circles.eq(index).addClass('bubble');
+    }, 1000);
+
+    /* For who animation */
+    $('.target').on('click', function() {
+        $('.target').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    /* Tabs */
+    $tabLinks = $('.tabs_link');
+    $tabLinks.on('click', function(e) {
+        e.preventDefault();
+        $tabLinks.removeClass('active');
+        $(this).addClass('active');
+
+        var index = $(this).data('tab');
+
+         $('.tab').removeClass('active');
+         $('.tab[data-tab="' + index + '"]').addClass('active');
+
+    })
 });
